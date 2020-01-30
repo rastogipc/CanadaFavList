9700edf6992b6814d02ab3c7799081cd79c79273
Prakash Rastogi <pkrastogi1@gmail.com>
2020-01-31 01:15:13 +0530

    Local Json File Reading

---
+++ CanadaFavList/CanadaFavList/AppConstant.swift
@@ -0,0 +1,9 @@
+//
+//  AppConstant.swift
+//  CanadaFavList
+//
+//  Created by Prakash Rastogi on 23/01/20.
+//  Copyright © 2020 Prakash Rastogi. All rights reserved.
+//
+
+import Foundation
---
+++ CanadaFavList/CanadaFavList/SwiftyHUDView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyHUDView.swift
+//  CanadaFavList
+//
+//  Created by Prakash Rastogi on 27/01/20.
+//  Copyright © 2020 Prakash Rastogi. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftyHUDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftyHUDView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftyHUDView()
+    }
+}
